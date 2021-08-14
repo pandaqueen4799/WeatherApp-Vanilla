792b857e517bb58299bcc473259eab58becdbfc6
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-08-14 13:10:43 -0400

    updated the time
    js still not working but it is connected cause when I do console.log and alerts it works

--- java.js
+++ java.js
@@ -1,14 +1,35 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+     if (hours < 10) {
+       hours = `0${hours}`;
+     }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
+
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind")
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
+  cityElement.innerHTMLL = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.windElement.speed;
+  windElement.innerHTML = Math.round(response.data.windElement.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b19af43e62b4a3c076b585af4be814dd";
