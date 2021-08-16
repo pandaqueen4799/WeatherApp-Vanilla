c470e5c065224019de2dffac8d813da3e54396a5
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-08-16 17:41:27 -0400

    fix js and html
    js works a little now but few thing still don't work

--- java.js
+++ java.js
@@ -100,7 +100,7 @@ q=New York&appid=${apiKey}&units=metric`;
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("city-input");
+  let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
 
@@ -123,13 +123,13 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
-let form = document.querySelector("search-form");
+let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displaycelsiusTemperature);
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
